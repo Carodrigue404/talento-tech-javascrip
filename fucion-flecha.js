402e6457171e48974efb126632116610f986e134
carodriguez404 <carodriguez404@gmail.com>
2025-08-12 19:31:39 -0500

    aprendidno como se hace una flecha

---
+++ fucion-flecha.js
@@ -0,0 +1,12 @@
+function nombreDeLaaccion(n1,n2){
+    console.log("los numero son :",n1,n2)
+}
+
+const variable = ()=>{}
+
+
+const imprimirNombre = (nombre)=>{
+    return"hola" + nombre;
+}
+resultado= imprimirNombre("edwin");
+console.log(resultado)
