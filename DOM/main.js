7a21cd3b7436a7d1934530af47db6394f4670102
carodriguez404 <carodriguez404@gmail.com>
2025-08-12 20:49:36 -0500

    agrege el archivo main.js

---
+++ DOM/main.js
@@ -0,0 +1,25 @@
+const v1= "tio ben"
+const v2= "recuerda la sigiente"
+const v3= "un gran poder requiere una gran responsabilidad"
+const v4= "ten ciudado con el duende verde"
+
+const spiderman=()=>{
+    if(peligro == true){
+        return `como decia el ${v1} ${v2} ${v3}`;
+    }else{
+        return `como decia el ${v1} ${v4}`;
+    }
+};
+//etiqueta de titulo
+const titulo = document.createElement("h1");
+titulo.textContent ="hola me llamo spiderman";
+document.body.appendChild(titulo);
+//crear boton desde javascrip
+const boton = document.createElement("button");
+const boton2 = document.createElement("button");
+
+boton.innerText="peligro";
+boton2.innerText="no peligro";
+
+document.body.appendChild(boton);
+document.body.appendChild(boton2);
